845f077cc93e65db86963464adb8c8a81bd26942
Ichionote <fabian31lucero@gmail.com>
2021-12-09 23:44:34 -0300

    fin del proyecto

--- usuarios/usuarios/settings/base.py
+++ usuarios/usuarios/settings/base.py
@@ -39,7 +39,9 @@ DJANGO_APPS = (
     'django.contrib.staticfiles',
 )
 
-LOCAL_APPS = ()
+LOCAL_APPS = (
+    'applications.users',
+)
 
 THIRD_PARTY_APPS = ()
 
@@ -96,6 +98,8 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+AUTH_USER_MODEL = 'users.User'
 
 
 # Internationalization
